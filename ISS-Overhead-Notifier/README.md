23bd35455a82976afa623868e1587e2c5fffa432
rayyan21d <rayyan9290@gmail.com>
2023-06-12 20:49:48 +0530

    kanye quote

---
+++ ISS-Overhead-Notifier/README.md
@@ -0,0 +1,8 @@
+# APIs
+
+## ISS Location API
+
+### Description
+This API returns the current location of the International Space Station (ISS) in the form of latitude and longitude. This app uses this API to determine the current location of the ISS and notify the user if the ISS is overhead.
+
+[!Kanye Quote]("./assets/kanye-quote.png")
--- ISS-Overhead-Notifier/main.py
+++ ISS-Overhead-Notifier/main.py
@@ -1,5 +1,13 @@
 import requests
 
 with requests.get(url="http://api.open-notify.org/iss-now.json") as response:
-    print(response.json)
+    response.raise_for_status() # Raise exception if error
     
+    data = response.json()
+    print(data)
+    
+    latitiude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
+    
+    iss_position = (latitiude, longitude)
+    print(iss_position)
